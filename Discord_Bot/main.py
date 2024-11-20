bcb1bf84e5bc935e4923e934e5215775bf7a04c8
Goes Dev <jcgoes.dev@gmail.com>
2024-11-19 23:31:10 -0300

    iniciando procedimentos

---
+++ Discord_Bot/main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from random import randint
+
+import Discord_Bot.VARIABLE as VARIABLE
+
+permissions = discord.Intents.default()
+permissions.message_content = True
+permissions.members = True
+bot = commands.Bot(command_prefix="-", intents=permissions)
+
+
+@bot.command()
+async def play(ctx: commands.Context):
+    die = randint(1, 6)
+    await ctx.send(f"Primeiro Dragão: **{die}**")
+
+
+@bot.command()
+async def m_help(ctx: commands.Context):
+    await ctx.reply(VARIABLE.ALL_COMMANDS)
+
+
+bot.run(VARIABLE.BOT)
--- Translator/translate.py
+++ Translator/translate.py
@@ -2,7 +2,7 @@ from deep_translator import GoogleTranslator
 
 tradutor = GoogleTranslator(source='pt', target='en')
 
-texto = 'Criado um app de tradução'
+texto = "Removendo"
 
 traducao = tradutor.translate(texto)
 
