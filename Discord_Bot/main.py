082abe365c2cbbfc762e646e73bb1cdd5362187f
Goes Dev <jcgoes.dev@gmail.com>
2024-11-28 12:54:10 -0300

    minor changes, project in stand by

--- Discord_Bot/main.py
+++ Discord_Bot/main.py
@@ -1,33 +1,25 @@
 import discord
-from discord.ext import commands
-from random import randint
 
 import VARIABLE
 
-permissions = discord.Intents.default()
-permissions.message_content = True
-permissions.members = True
-bot = commands.Bot(command_prefix="-", intents=permissions)
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 
-@bot.command()
-async def play(ctx: commands.Context):
-    die = randint(1, 6)
-    await ctx.send(f"Primeiro Dragão: **{die}**")
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
 
-@bot.command()
-async def m_help(ctx: commands.Context):
-    await ctx.reply(VARIABLE.ALL_COMMANDS)
-
-
-@bot.event
-async def on_message(msg: discord.Message):
-    await bot.process_commands(msg)
-    autor = msg.author
-    if autor.bot:
+@client.event
+async def on_message(message):
+    if message.author == client.user:
         return
-    # await msg.reply("Olá, sou a Marriene, um BOT para a Guilda Velho Dragão")
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 
-bot.run(VARIABLE.BOT)
+client.run(VARIABLE.BOT)
