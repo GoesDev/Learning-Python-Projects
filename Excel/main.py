a5eae194b02d4c4f6028e6ea9fcc2ec4a76fc1d8
Goes Dev <jcgoes.dev@gmail.com>
2024-11-28 17:50:50 -0300

    minor changes

--- Excel/main.py
+++ Excel/main.py
@@ -1,32 +1,8 @@
 import pandas as pd
+from main_variable import coluna, colunas_para_deletar, valor
 
 arquivo_excel = 'orcamento.xlsx'
 df = pd.read_excel(arquivo_excel, engine='openpyxl')
-
-
-coluna = ['ID AGENDA',
-          'STATUS DA ATIVIDADE',
-          'STATUS DA ATIVIDADE',
-          'SITUAÇÃO DO ORÇAMENTO',
-          'SITUAÇÃO DO ORÇAMENTO',
-          'SITUAÇÃO DO ORÇAMENTO']
-
-valor = ['PRODUTIZADO',
-         'PENDÊNCIA',
-         'CANCELADA',
-         'FINALIZADO',
-         'SUPERVISOR VERIFICANDO',
-         'N/A']
-
-colunas_para_deletar = [
-    'REG OBRGA',
-    'Nº PROJETO',
-    'LIDER',
-    'MATERIAL SGCI',
-    'SERVIÇO SGCI',
-    'SITE'
-]
-
 
 for i in range(6):
     df = df[df[coluna[i]] != valor[i]]
@@ -35,5 +11,19 @@ df = df[df['SITUAÇÃO DO ORÇAMENTO'].apply(
     lambda x: pd.notna(x) and x.strip() != '')]
 df = df[df['STATUS DA ATIVIDADE'].apply(
     lambda x: pd.notna(x) and x.strip() != '')]
+
+n_colunas = len(colunas_para_deletar)
+
+count = 0
+
+for c in range(n_colunas):
+    del df[colunas_para_deletar[count]]
+    count += 1
+
+df = df[df['ATP'] != 'SP4']
+df = df[df['SUBPROCESSO'].isnull() | (df['SUBPROCESSO'] == '')]
+
+df['ATP'] = df['ATP'].astype(str)
+df = df[~df['ATP'].str.startswith('OSX-')]
 
 df.to_excel('_arquivo.xlsx', index=False, engine='openpyxl')
