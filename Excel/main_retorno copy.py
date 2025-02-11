801432857b0511fd218471822c9c3acc3be478ae
Goes Dev <jcgoes.dev@gmail.com>
2025-02-11 20:39:05 -0300

    finalizando o script

--- Excel/main_retorno copy.py
+++ Excel/main_retorno copy.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-arquivo_excel = 'retorno.xlsx'
+arquivo_excel = 'b2b-01_01_2025-05_02_2025.xls'
 df = pd.read_excel(arquivo_excel, engine='openpyxl')
 
-todas_as_colunas = df.columns.tolist()
+todas_as_colunas = list(df.columns)
 
 count = 0
 colunas_para_deletar = []
@@ -29,19 +29,20 @@ colunas_para_n_deletar = [
     'REFERENCIA'
 ]
 
+count = 0
 
 # CRIA UMA LISTA COM AS COLUNAS QUE DEVEM SER EXCLUIDAS
-for coluna in todas_as_colunas:
-    if coluna not in colunas_para_n_deletar:
-        colunas_para_deletar.append(coluna)
-
+for c in todas_as_colunas:
+    if c not in colunas_para_n_deletar:
+        colunas_para_deletar.append(count)
+    count += 1
 
 # DELETA AS COLUNAS ARMAZENADAS NA LISTA
-df = df.drop(colunas_para_deletar, axis=1)
+df = df.drop(df.columns[colunas_para_deletar], axis=1)
 
 
-# # EXCLUINDO TODAS AS LINHAS QUE TENHAM SUPERVISOR VERIFICANDO NA
-# # COLUNA ORÇAMENTO
+# EXCLUINDO TODAS AS LINHAS QUE TENHAM SUPERVISOR VERIFICANDO NA
+# COLUNA ORÇAMENTO
 coluna_excluir = 'SITUAÇÃO ORÇAMENTO'
 linha_excluir = 'SUPERVISOR VERIFICANDO'
 df = df[df[coluna_excluir] != linha_excluir]
@@ -54,5 +55,7 @@ df = df[df['TOTAL C/ IMPOSTO'] == 0]
 # STATUS ATIVIDADE
 df = df[df['STATUS DA ATIVIDADE'] == 'CONCLUSIVA']
 
+
+print(df.head(100))
 
 df.to_excel('retorno_final.xlsx', index=False, engine='openpyxl')
